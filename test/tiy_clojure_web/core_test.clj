439f1bb1e8927e525c24ca09122037fd9abb34a1
Taylor Miller <trmiller10@gmail.com>
2016-06-02 18:22:41 -0400

    initial commit

---
+++ test/tiy_clojure_web/core_test.clj
@@ -0,0 +1,7 @@
+(ns tiy-clojure-web.core-test
+  (:require [clojure.test :refer :all]
+            [tiy-clojure-web.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
